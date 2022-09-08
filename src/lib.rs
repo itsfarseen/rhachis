5c6e56b2eb01461f1065b798672f604856ba5d5f
SalsaGal <fielddashos@gmail.com>
2022-09-08 23:03:31 +1000

    Add window

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use winit::{event_loop::{EventLoop, ControlFlow}, window::WindowBuilder, event::{Event, WindowEvent}};
+
 pub trait Game {
     fn init() -> Self;
 }
@@ -9,5 +11,16 @@ pub trait GameExt {
 impl<T> GameExt for T
 where T: Game {
     fn run() {
+        let event_loop = EventLoop::new();
+        let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+        event_loop.run(move |event, _, control_flow| match event {
+            Event::MainEventsCleared => window.request_redraw(),
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                _ => {},
+            }
+            _ => {}
+        });
     }
 }
