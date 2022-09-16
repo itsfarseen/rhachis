6a7473ee451ff1c329b2933031906152392ce959
SalsaGal <fielddashos@gmail.com>
2022-09-16 22:46:15 +1000

    Add perlin_2d

--- src/rand.rs
+++ src/rand.rs
@@ -3,6 +3,10 @@ use std::{
     time::SystemTime,
 };
 
+use glam::Vec2;
+
+use crate::math::lerp;
+
 pub struct Noise {
     pub seed: u32,
     pub index: u32,
@@ -65,4 +69,33 @@ impl Default for Noise {
     fn default() -> Self {
         Self::new()
     }
+}
+
+pub fn perlin_2d(noise: &Noise, pos: Vec2) -> f32 {
+    fn dot_gradient(noise: &Noise, cell: Vec2, pos: Vec2) -> f32 {
+        let gradient = Vec2::new(
+            noise.get(2 * cell.x as u32) as f32,
+            noise.get(2 * cell.x as u32 + 1) as f32,
+        )
+        .normalize();
+
+        let distance_vector = pos - cell;
+
+        distance_vector.dot(gradient)
+    }
+
+    let grid_cell_start = pos.floor();
+    let grid_cell_end = grid_cell_start + Vec2::new(1.0, 1.0);
+
+    let lerp_weights = pos - grid_cell_start;
+
+    let top_left_gradient = dot_gradient(noise, grid_cell_start, pos);
+    let top_right_gradient = dot_gradient(noise, Vec2::new(grid_cell_start.x, grid_cell_end.y), pos);
+    let bottom_left_gradient = dot_gradient(noise, Vec2::new(grid_cell_end.x, grid_cell_start.y), pos);
+    let bottom_right_gradient = dot_gradient(noise, grid_cell_end, pos);
+
+    let top_lerp = lerp(top_left_gradient, top_right_gradient, lerp_weights.x);
+    let bottom_lerp = lerp(bottom_left_gradient, bottom_right_gradient, lerp_weights.x);
+
+    lerp(top_lerp, bottom_lerp, lerp_weights.y)
 }
