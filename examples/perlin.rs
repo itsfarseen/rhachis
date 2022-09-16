47379b61066d4bd644d4991d141289e1fe45e006
SalsaGal <fielddashos@gmail.com>
2022-09-16 23:49:38 +1000

    Render voxels in perlin demo

--- examples/perlin.rs
+++ examples/perlin.rs
@@ -1,4 +1,6 @@
-use glam::Mat4;
+use std::f32::consts::TAU;
+
+use glam::{Mat4, Quat};
 use rhachis::{
     rand::Noise,
     renderers::{Model, SimpleRenderer, Transform},
@@ -17,7 +19,12 @@ impl Game for PerlinExample {
     fn init(data: &rhachis::GameData) -> Self {
         let mut renderer = SimpleRenderer::new(
             data,
-            Mat4::orthographic_lh(-1.0, 1.0, -1.0, 1.0, 0.1, 100.0),
+            Mat4::perspective_rh(
+                TAU / 4.0,
+                data.get_window_size().x as f32 / data.get_window_size().y as f32,
+                0.1,
+                100.0,
+            ),
         );
 
         renderer.models.push(
@@ -47,11 +54,13 @@ fn terrain_transforms(noise: &Noise) -> Vec<Transform> {
         for y in 0..TERRAIN_HEIGHT {
             let height = (x + y) as f32;
 
-            to_ret.push(Transform::translation((
-                x as f32,
-                height,
-                y as f32,
-            ).into()));
+            to_ret.push(
+                Transform::translation((
+                    x as f32,
+                    height - 2.0,
+                    -(y as f32 + 3.0),
+                ).into()).with_rotation(Quat::from_rotation_y(TAU / 4.0)).with_scale((0.5, 0.5, 0.5).into())
+            );
         }
     }
 
