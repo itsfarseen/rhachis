bd4350414c24ef0577252219891bb959f3150ef8
Farseen <oss@itsfarseen.anonaddy.me>
2022-10-05 16:05:39 +0530

    Replace lerp with smootherstep in perlinimage example

--- examples/perlinimage.rs
+++ examples/perlinimage.rs
@@ -2,7 +2,7 @@ use glam::{Vec2, Vec3};
 use image::{Rgba, RgbaImage};
 use rhachis::{
     graphics::Renderer,
-    math::lerp,
+    math::smootherstep,
     rand::{perlin_2d, Noise},
     renderers::{Model, SimpleRenderer, Texture, Transform},
     *,
@@ -24,7 +24,7 @@ impl Game for PerlinImage {
 
         for x in 0..IMAGE_WIDTH {
             for y in 0..IMAGE_HEIGHT {
-                let perlin = perlin_2d(&noise, Vec2::new(x as f32, y as f32) / 64.0, lerp);
+                let perlin = perlin_2d(&noise, Vec2::new(x as f32, y as f32) / 64.0, smootherstep);
                 let value = ((perlin + 1.0) * 127.0) as u8;
                 image.put_pixel(x, y, Rgba([value, value, value, 255]));
             }
